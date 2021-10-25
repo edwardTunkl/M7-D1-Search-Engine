343a8fbd00e0a84d05c9f061b682d53c91b9750e
edwardTunkl <eddytunkl@googlemail.com>
2021-10-25 19:27:32 +0200

    more styling

--- search-engine/src/Components/Searchbar.jsx
+++ search-engine/src/Components/Searchbar.jsx
@@ -2,63 +2,86 @@ import {
   Navbar,
   Nav,
   NavDropdown,
-  Form,
+  InputGroup,
   FormControl,
-  Button,
   Container,
 } from "react-bootstrap";
 import React from "react";
 import { withRouter } from "react-router";
 import "../search.css";
 import { setQueryAction, setSearchByAction } from "../actions";
-import {  useDispatch } from 'react-redux'
 import { useHistory } from "react-router-dom";
-
+import { useSelector, useDispatch } from "react-redux";
 
 const Searchbar = () => {
-
- const dispatch = useDispatch()
- const history = useHistory()
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   return (
-    
     <Navbar bg="info" expand="lg">
       <Container className="px-4">
-        <Navbar.Brand id="engine-name"><span className="title">80`s</span> Search-Engine</Navbar.Brand>
+        <Navbar.Brand id="engine-name">
+          <span className="title">80ies</span> Search-Engine
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link className="text-warning shaddow" style={{fontSize:"1.3rem"}} onClick={() => history.push(`/`)}>Home</Nav.Link>
-            <Nav.Link className="text-warning shaddow" style={{fontSize:"1.3rem"}} onClick={() => history.push(`/favourites`)}>Favourites</Nav.Link>
-            <NavDropdown title="Select Search" id="basic-nav-dropdown" style={{fontSize:"1.5rem"},{marginLeft:"15rem"}}>
-              <NavDropdown.Item onClick={() => dispatch(setSearchByAction("Title"))} > 
-              Title
+            <Nav.Link
+              className="text-warning shaddow ml-2"
+              id="navi"
+             
+              onClick={() => history.push(`/`)}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              id="navi"
+              className="text-warning shaddow"
+              
+              onClick={() => history.push(`/favourites`)}
+            >
+              Favourites
+            </Nav.Link>
+            <NavDropdown
+              title="Select Search"
+              id="basic-nav-dropdown"
+              style={({ fontSize: "1.5rem" }, { marginLeft: "8rem" })}
+              className="mt-2 mr-2"
+            >
+              <NavDropdown.Item id="droppi"
+                onClick={() => dispatch(setSearchByAction("Title"))}
+              >
+                Title
               </NavDropdown.Item>
-              <NavDropdown.Item onClick={() => dispatch(setSearchByAction("Category"))} > 
-              Category
+              <NavDropdown.Item id="droppi"
+                onClick={() => dispatch(setSearchByAction("Category"))}
+              >
+                Category
               </NavDropdown.Item>
-              <NavDropdown.Item onClick={() => dispatch(setSearchByAction("Company"))} > 
-              Company
+              <NavDropdown.Item id="droppi"
+                onClick={() => dispatch(setSearchByAction("Company"))}
+              >
+                Company
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
-          <Form inline>
+          <InputGroup className="mb-3">
             <FormControl
               type="text"
               placeholder="Search"
-              className="mr-sm-2"
+              className="mr-sm-2 mt-3"
               onKeyDown={(e) => {
                 if (e.key === "Enter") {
                   e.preventDefault();
                   dispatch(setQueryAction(e.target.value));
                 }
               }}
+              onClick={() => history.push("/")}
             />
-          </Form>
+          </InputGroup>
         </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      
+      </Container>
+    </Navbar>
   );
 };
-export default withRouter(Searchbar)
+export default withRouter(Searchbar);
